5e8b8f399402e5210cf39fd9bf494f06177cce6d
Steven Mentzer <stevenfmentzer@gmail.com>
2023-11-02 11:23:40 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left>0){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left<360){
+    dodger.style.left = `${left + 1}px`;
+    }
+}
